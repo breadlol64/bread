35098c82d3ffa3c0218031279bf8853f1e613001
Artur <breadlol64@gmail.com>
2025-09-27 18:40:08 +0300

    setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,52 @@
 <script lang="ts">
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+    import "../app.css";
+    import favicon from "$lib/assets/favicon.svg";
+    import { page } from "$app/state";
+
+    import {
+        DarkMode,
+        Footer,
+        FooterCopyright,
+        GradientButton,
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+    } from "flowbite-svelte";
+
+    let { children } = $props();
+
+    let activeUrl = $derived(page.url.pathname);
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+    <link rel="icon" href={favicon} />
 </svelte:head>
 
+<Navbar>
+    <NavBrand href="/">
+        <span
+            class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+            >bread</span
+        >
+        <DarkMode />
+    </NavBrand>
+    <div class="flex md:order-2">
+        <GradientButton color="pinkToOrange">login</GradientButton>
+    </div>
+    <NavHamburger />
+    <NavUl {activeUrl}>
+        <NavLi href="/">Home</NavLi>
+        <NavLi href="/bober">BOBER KOMBAET</NavLi>
+        <NavLi href="/y">Y</NavLi>
+        <NavLi href="/bobrtube">BobrTube</NavLi>
+        <NavLi href="/tiptop">tiptop</NavLi>
+    </NavUl>
+</Navbar>
+
 {@render children?.()}
+
+<!-- <Footer>
+    <FooterCopyright by="breadlol64" year={2025} class=""/>
+</Footer> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { Alert } from "flowbite-svelte";
+    import axios from "axios";
+    import { onMount } from "svelte";
+
+    let message = $state("");
+
+    onMount(async () => {
+        try {
+            const response = await axios.get("/api/hello");
+            message = response.data.msg;
+        } catch (error) {
+            console.error(error);
+        }
+    });
+</script>
+
+<div class="p-8">
+    <Alert>
+        <span class="font-medium">Info alert!</span>
+        {message}
+    </Alert>
+</div>
